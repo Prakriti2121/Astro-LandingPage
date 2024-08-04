f34ddc570994463bfe0822d64f7d3c190f993819
Prakriti Adhikari <prakritiadhikari2121@gmail.com>
2024-08-04 23:18:06 +0545

    ContactUs Added

---
+++ src/components/ContactUs.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Button from "./Button";
+
+const ContactUs = () => {
+  return (
+    <section
+      id="contactus"
+      className="mt-16 mx-4 sm:mx-8 md:mx-12 lg:mx-32 p-4 sm:p-6 rounded-lg "
+    >
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2 p-4 sm:p-6 flex flex-col space-y-6">
+          <div className="space-y-3 text-xl md:text-2xl lg:text-4xl text-primary font-family font-sans">
+          <div className="text-2xl md:text-3xl lg:text-4xl text-primary font-family font-sans">
+            <h3>Voice for Being Together</h3>
+          </div>
+            <p className="font-sans text-sm lg:text-base text-center md:text-left text-black py-2">
+              We, together can make this earth beautiful and need to make a
+              voice united.
+            </p>
+          </div>
+
+          <form className="flex flex-col space-y-4">
+            <label htmlFor="email" className="font-sans text-lg">
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Your Email"
+                className="w-full p-2 border border-gray-400 rounded-lg"
+                required
+              />
+            </label>
+            <label htmlFor="message" className="font-sans text-lg">
+              <textarea
+                id="message"
+                name="message"
+                rows="4"
+                placeholder="Your Message"
+                className="w-full p-2 border border-gray-400 rounded-lg"
+                required
+              ></textarea>
+            </label>
+            <div className="pt-2 flex justify-center md:justify-start">
+              <Button variant="rounded">Submit</Button>
+            </div>
+          </form>
+        </div>
+        <div className="w-full md:w-1/2 p-4 sm:p-6 flex justify-center items-center">
+          <img
+            src="../src/assets/prosandcons.png"
+            alt="Pros and Cons"
+            className="w-full h-auto object-cover rounded-lg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ContactUs;
